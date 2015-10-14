ac1586f47ff0aae59f45cf433b4aaba9fc8bb8f9
mvijayasekhar <munagalavijayasekhar@gmail.com>
2015-10-14 14:45:34 +0530

    Adding SCTP samples

--- Linux/Simple_SCTP/Header/SimpleSCTP.h
+++ Linux/Simple_SCTP/Header/SimpleSCTP.h
@@ -2,23 +2,23 @@
 #define _MY_HEADER_
 
 
-#include    <stdio.h>
-#include    <stdlib.h>
-#include    <sys/types.h>
-#include    <sys/socket.h>
-#include    <string.h>
-#include    <errno.h>
-#include    <netinet/ip.h>
-#include    <arpa/inet.h>
-#include    <unistd.h>
-
-
-#define PORT 8001
-#define MAX 100
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netinet/sctp.h>
+#define MAX_BUFFER 1024
+#define PORT_NUM 8001
 #define Q_LEN 10
 
 typedef struct sockaddr SOCK_ADDR;
 typedef struct sockaddr_in SOCK_ADDR_IN;
-
+typedef struct sctp_initmsg SCTP_INIT_MSG;                                                  
+typedef	struct sctp_sndrcvinfo SCTP_SNDRCVINFO;
 #endif
 
