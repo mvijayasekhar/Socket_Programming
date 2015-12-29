924e8a4fd0499353d7bcec2aae2796bb277ac9f5
mvijayasekhar <munagalavijayasekhar@gmail.com>
2015-12-29 12:00:02 +0530

    Adding TCP Header

---
+++ Linux/Simple_RAW/Header/TcpHeader.h
@@ -0,0 +1,24 @@
+#ifndef __TCP_HEADER__
+#define __TCP_HEADER__
+
+struct tcp_hdr{                                                             
+	unsigned short int src_port;                                            
+	unsigned short int dest_port;                                           
+	unsigned int seq_no;                                                    
+	unsigned int ack_no;                                                    
+	unsigned char res_bits:4;                                               
+	unsigned char h_len:4;                                                  
+	unsigned char fin:1;                                                    
+	unsigned char syn:1;                                                    
+	unsigned char rst:1;                                                    
+	unsigned char psh:1;                                                    
+	unsigned char ack:1;                                                    
+	unsigned char urg:1;                                                    
+	unsigned char ece:1;                                                    
+	unsigned char cwr:1;                                                    
+	unsigned short int win_size;                                            
+	unsigned short int check_sum;                                           
+	unsigned short int urg_ptr;                                             
+} *tcp_hdr;
+
+#endif
